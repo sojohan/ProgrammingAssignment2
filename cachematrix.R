beb96f686da16df26b9e415641a65bbf82a1d91c
Soren Johansen <sojohan@yahoo.dk>
2015-09-25 11:02:43 +0200

    2. Test the line to github

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,18 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { 
+  m <- NULL
+set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+get <- function() x
+setsolve <- function(solve) m <<- solve
+getsolve <- function() m
+list(set = set, get = get,
+     setsolve = setsolve,
+     getsolve = getsolve)
 
 }
 
